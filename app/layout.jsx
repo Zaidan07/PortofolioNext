b1e6c7253d03e79828e96b24214314390a5bf9eb
Zaidan07 <zaidanxgaming@gmail.com>
2024-12-09 09:02:08 +0700

    feat : metadata seo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 //components
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -8,31 +9,29 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Zaidan Nabih | Frontend Web Developer", 
-  description: "Portofolio Zaidan",
-  keywords: "portofolio, zaidan, masdan, jidun",
-  openGraph: {
-    title: "My Portfolio",
-    description: "Portofolio Zaidan",
-    type: "website",
-    url: "https://zaidannabih.vercel.app/",
-    images: [
-      {
-        url: "https://yourdomain.com/preview-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Preview image for My Portfolio",
-      },
-    ],
-  },
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <title>Zaidan Nabih | Frontend Developer</title>
+        <meta name="description" content="Portofolio Zaidan" />
+        <meta
+          name="keywords"
+          content="portofolio, zaidan, developer, designer, zaidanmuhi"
+        />
+        <meta name="author" content="Zaidan" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Zaidan Nabih | Frontend Developer" />
+        <meta property="og:description" content="Portofolio Zaidan" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://zaidannabih.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://yourdomain.com/preview-image.png"
+        />
+      </Head>
       <body className={outfit.className}>
-        <ThemeProvider attribute="class" defaultTheme='light'>
+        <ThemeProvider attribute="class" defaultTheme="light">
           <Header />
           {children}
           <Footer />
