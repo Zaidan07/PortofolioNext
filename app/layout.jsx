afe05254fcef0dd78f1f245a9b74b92fa7e6a7e6
Zaidan07 <zaidanxgaming@gmail.com>
2024-12-09 09:09:12 +0700

    feat : metadata seo

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,21 +14,15 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <Head>
         <title>Zaidan Nabih | Frontend Developer</title>
-        <meta name="description" content="Portofolio Zaidan" />
-        <meta
-          name="keywords"
-          content="portofolio, zaidan, developer, designer, zaidanmuhi"
-        />
-        <meta name="author" content="Zaidan" />
-        <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="Zaidan Nabih | Frontend Developer" />
-        <meta property="og:description" content="Portofolio Zaidan" />
+        <meta name="description" content="Portofolio Masdan: Web Developer dan Desainer Profesional" />
+        <meta name="keywords" content="portofolio, masdan, web developer, jidunmuhi, nextjs, react" />
+        <meta name="author" content="Masdan" />
+        <meta property="og:title" content="Masdan Portfolio" />
+        <meta property="og:description" content="Portofolio Masdan: Web Developer dan Desainer Profesional" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://zaidannabih.vercel.app/" />
-        <meta
-          property="og:image"
-          content="https://yourdomain.com/preview-image.png"
-        />
+        <meta property="og:image" content="https://yourdomain.com/preview-image.png" />
+        <meta name="robots" content="index, follow" />
       </Head>
       <body className={outfit.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+    siteUrl: 'https://zaidannabih.vercel.app', // Ganti dengan domain Anda
+    generateRobotsTxt: true,
+  };
