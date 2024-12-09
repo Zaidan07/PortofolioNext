528d153ec3b439ff6aaa795fa4646d8bbce35832
Zaidan07 <zaidanxgaming@gmail.com>
2024-12-09 10:47:49 +0700

    feat : metadata seo

--- app/api/sitemap/route.js
+++ app/api/sitemap/route.js
@@ -1,27 +1,23 @@
-export async function GET(request) {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://zaidannabih.vercel.app/</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://zaidannabih.vercel.app/about</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://zaidannabih.vercel.app/projects</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-</urlset>`;
+import { NextResponse } from "next/server";
 
-  return new Response(sitemap, {
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://zaidannabih.vercel.app/</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>daily</changefreq>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>https://zaidannabih.vercel.app/about</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  </urlset>`;
+
+  return new NextResponse(sitemap, {
     headers: {
       "Content-Type": "application/xml",
     },
--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteUrl: "https://zaidannabih.vercel.app", // Ganti dengan URL website Anda
-  generateRobotsTxt: true, // Untuk menghasilkan robots.txt secara otomatis
-  sitemapSize: 5000, // Jumlah maksimum URL per file sitemap
-  changefreq: "daily", // Frekuensi update halaman (opsional)
-  priority: 0.7, // Prioritas halaman (opsional)
-  };
