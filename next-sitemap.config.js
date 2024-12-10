1501f733acbb6412a122d2696cfa38c5dce820ca
Zaidan07 <zaidanxgaming@gmail.com>
2024-12-10 07:39:49 +0700

    feat : sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://zaidannabih.vercel.app',
+    siteUrl: 'https://zaidannabih.vercel.app',
     generateRobotsTxt: true, // (Optional) Menghasilkan robots.txt
     sitemapSize: 7000, // Batasi jumlah URL per file sitemap
 };
