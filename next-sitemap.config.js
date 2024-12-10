64420afbb36cfbf7f445848c9a84986713bcec1b
Zaidan07 <zaidanxgaming@gmail.com>
2024-12-10 08:31:01 +0700

    feat : text

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://zaidannabih.vercel.app', // Ganti dengan domain Anda
+    siteUrl: 'https://muhzaidan.vercel.app', // Ganti dengan domain Anda
     generateRobotsTxt: true, // Untuk membuat robots.txt
     sitemapSize: 5000,
   };
